674146533e5942c23824dae029fbb02334b9bb7d
Mattu0408 <hardik.pandya9393@gmail.com>
2025-11-21 20:47:39 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-30 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
-
+echo "we are leaning mergerconflict topic"
+echo "we are leaning mergerconflict topics"
