63fa9845c2541617e4ce47e424c0d5d25d4436ab
Mattu0408 <hardik.pandya9393@gmail.com>
2025-11-21 20:34:54 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,9 +1,6 @@
 echo "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
+echo "We are from batch-30 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
 echo "Adding new line"
-echo "I am from Devops and cloud teams"
-echo "we are learing Aiops, Machine Learnign"
-echo "This is the Best coaching center"
 
