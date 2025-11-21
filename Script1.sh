c6e6c84e2f3d1cf733aeb2e03d410d05c6a4990f
Hardik <hardik.pandya9393@gmail.com>
2025-11-21 19:57:46 +0530

    i hae done changes for incident INC0123456

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,7 @@ echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
 echo "Adding new line"
-echo "I am from Devops and cloud team"
+echo "I am from Devops and cloud teams"
+echo "we are learing Aiops, Machine Learnign"
+echo "This is the Best coaching center"
 
